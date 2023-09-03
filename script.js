c6533d3a8a73301ea66e6322f89d1ee41afd29cb
Manhad Khan <manhad.majin@outlook.com>
2023-09-03 16:10:13 +0530

    Added an operate function which calls the add, substract, multiply or divide function and passes it to the two given numbers.

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function divide(a, b) {
         return a / b;
     }
 }
+
+//A function to call one of the above function for operation
+
+function operate(operator, num1, num2) {
+    if(operator === '+') {
+        return add(num1, num2);
+    }
+    else if(operator === '-') {
+        return substract(num1, num2);
+    }
+    else if(operator === '*') {
+        return multiply(num1, num2);
+    }
+    else if(operator === '/') {
+        return divide(num1, num2);
+    }
+    else {
+        return "Invalid operator";
+    }
+}
