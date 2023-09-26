cb95cb9302197f304475413d8e8459f6c202b563
mojave <102758567+manhadkhan@users.noreply.github.com>
2023-09-27 00:38:04 +0530

    added functionality to do multiple operations at once and update the display to 0 if clear button is clicked.

--- script.js
+++ script.js
@@ -65,3 +65,63 @@ clearButton.addEventListener('click', () => {
     displayData = ""; //when the clear button is clicked, display data value will be set to an empty string.
     display.textContent = displayData; //assigning displayData value to display
 })
+
+// Initialize variables to store the current number, operator, and the accumulated result
+let currentNumber = "";
+let currentOperator = "";
+let accumulatedResult = null;
+
+// Function to update the display
+function updateDisplay() {
+    display.textContent = currentNumber !== "" ? currentNumber : accumulatedResult !== null ? accumulatedResult : "0";
+}
+
+// Event listener for the equals button
+equalsButton.addEventListener('click', () => {
+    if (currentOperator && currentNumber) {
+        const num1 = parseFloat(currentNumber);
+        if (accumulatedResult === null) {
+            accumulatedResult = num1;
+        } else {
+            accumulatedResult = operate(currentOperator, accumulatedResult, num1);
+        }
+        currentNumber = "";
+        currentOperator = "";
+        updateDisplay();
+    }
+});
+
+// Event listener for operators
+document.querySelectorAll('.btn-operator').forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', () => {
+        if (currentNumber) {
+            if (accumulatedResult === null) {
+                accumulatedResult = parseFloat(currentNumber);
+            } else {
+                const num1 = parseFloat(currentNumber);
+                accumulatedResult = operate(currentOperator, accumulatedResult, num1);
+            }
+            currentOperator = operatorBtn.getAttribute('data-num');
+            currentNumber = "";
+            updateDisplay();
+        }
+    });
+});
+
+// Event listener for numbers
+document.querySelectorAll('.btn-number').forEach(numberBtn => {
+    numberBtn.addEventListener('click', () => {
+        const number = numberBtn.getAttribute('data-num');
+        currentNumber += number;
+        updateDisplay();
+    });
+});
+
+// Clear the display and reset variables
+clearButton.addEventListener('click', () => {
+    currentNumber = "";
+    currentOperator = "";
+    accumulatedResult = null;
+    updateDisplay();
+});
+    
